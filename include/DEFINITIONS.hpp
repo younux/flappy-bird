4656ddb5bf7f4426281727f595240bba05deb8ea
younux <benhoumich.younes@gmail.com>
2018-01-20 23:08:05 +0100

    Change file location in DEFINNITIONS.hpp

--- include/DEFINITIONS.hpp
+++ include/DEFINITIONS.hpp
@@ -10,44 +10,44 @@
 
 #define SPLASH_STATE_SHOW_TIME 3.0
 
-#define SPLASH_SCENE_BACKGROUND_FILEPATH "./resources/res/Splash Background.png"
+#define SPLASH_SCENE_BACKGROUND_FILEPATH "../../resources/res/Splash Background.png"
 #define SPLASH_SCENE_BACKGROUND_NAME "Splash State Background"
 
-#define MAIN_MENU_BACKGROUND_FILEPATH "./resources/res/sky.png"
+#define MAIN_MENU_BACKGROUND_FILEPATH "../../resources/res/sky.png"
 #define MAIN_MENU_BACKGROUND_NAME "Main Menu Background"
 
-#define GAME_BACKGROUND_FILEPATH "./resources/res/sky.png"
+#define GAME_BACKGROUND_FILEPATH "../../resources/res/sky.png"
 #define GAME_BACKGROUND_NAME "Game Background"
 
-#define GAME_OVER_BACKGROUND_FILEPATH "./resources/res/sky.png"
+#define GAME_OVER_BACKGROUND_FILEPATH "../../resources/res/sky.png"
 #define GAME_OVER_BACKGROUND_NAME "Game Over"
 
 
-#define GAME_TITLE_FILEPATH "./resources/res/title.png"
+#define GAME_TITLE_FILEPATH "../../resources/res/title.png"
 #define GAME_TITLE_NAME "Game Title"
 
-#define PLAY_BUTTONN_FILEPATH "./resources/res/PlayButton.png"
+#define PLAY_BUTTONN_FILEPATH "../../resources/res/PlayButton.png"
 #define PLAY_BUTTONN_NAME "Play Button"
 
-#define PIPE_UP_FILEPATH "./resources/res/PipeUp.png"
+#define PIPE_UP_FILEPATH "../../resources/res/PipeUp.png"
 #define PIPE_UP_NAME "Pipe Up"
 
-#define PIPE_DOWN_FILEPATH "./resources/res/PipeDown.png"
+#define PIPE_DOWN_FILEPATH "../../resources/res/PipeDown.png"
 #define PIPE_DOWN_NAME "Pipe Down"
 
-#define LAND_FILEPATH "./resources/res/land.png"
+#define LAND_FILEPATH "../../resources/res/land.png"
 #define LAND_NAME "Land"
 
-#define BIRD_FRAME_1_FILEPATH "./resources/res/bird-01.png"
+#define BIRD_FRAME_1_FILEPATH "../../resources/res/bird-01.png"
 #define BIRD_FRAME_1_NAME "Bird Frame 1"
 
-#define BIRD_FRAME_2_FILEPATH "./resources/res/bird-02.png"
+#define BIRD_FRAME_2_FILEPATH "../../resources/res/bird-02.png"
 #define BIRD_FRAME_2_NAME "Bird Frame 2"
 
-#define BIRD_FRAME_3_FILEPATH "./resources/res/bird-03.png"
+#define BIRD_FRAME_3_FILEPATH "../../resources/res/bird-03.png"
 #define BIRD_FRAME_3_NAME "Bird Frame 3"
 
-#define BIRD_FRAME_4_FILEPATH "./resources/res/bird-04.png"
+#define BIRD_FRAME_4_FILEPATH "../../resources/res/bird-04.png"
 #define BIRD_FRAME_4_NAME "Bird Frame 4"
 
 #define PIPE_MOVEMENT_SPEED 200.0f
